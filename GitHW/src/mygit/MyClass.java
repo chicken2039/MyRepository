798741438a688880bbefeca6b78ef59ee64943b7
SeongMin Lee <zoq2039@gmail.com>
2025-12-04 13:52:47 +0900

    First Commiee in dev branch

--- GitHW/src/mygit/MyClass.java
+++ GitHW/src/mygit/MyClass.java
@@ -4,5 +4,7 @@ public class MyClass {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
 		System.out.println("Kore wa sekando bazion!");
+		YourClass your = new YourClass();
+		your.sayHello("Comrade!");
 	}
 }
